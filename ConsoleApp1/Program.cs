e4dfecf58f109d9cf490cf8b2d69ba66b64b570a
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:55:28 +0800

    更新

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double total_cost = 0;
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            total_cost = meal_cost + tip_percent + tax_percent;
+            Console.WriteLine(total_cost);
+            meal_cost = 12.0;
+            tip_percent = 20;
+            tax_percent = 8;
+            total_cost = meal_cost + tip_percent + tax_percent;
+            Console.WriteLine(total_cost);
         }
     }
 }
